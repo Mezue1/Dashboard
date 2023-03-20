a79b715d50f0b56ec4d16cb347ee752392319934
Mezue1 <chubamezue@hotmail.co.uk>
2023-03-20 09:28:54 +0000

    Fixed bugs

--- client/src/index.js
+++ client/src/index.js
@@ -3,25 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
-import globalReducer from "/state";
+import globalReducer from "state";
 import { Provider } from "react-redux";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { api } from "/state/api";
 
 const store = configureStore({
-  reducer: {
-    global: globalReducer,
-    [api.reducerPath]: api.reducer,
-  },
-  middleware: (getDefault) => getDefault().concat(api.middleware),
+    reducer: {
+        global: globalReducer,
+    },
 });
-setupListeners(store.dispatch);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
